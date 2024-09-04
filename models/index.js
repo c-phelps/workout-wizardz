af468a3841fdd1e668386f586cdeccb059ca567a
Halcyon <hashem_elmaghrabi@yahoo.com>
2024-09-03 21:28:23 -0400

    edited content in index.js file in models folder

--- models/index.js
+++ models/index.js
@@ -1,3 +1,4 @@
 const User = require("./models/User.js");
 const Exercise = require("./models/Exercise.js");
 const Workout = require("./models/Workout.js");
+const WorkoutExercises = require("./models/WorkoutExercises.js");
