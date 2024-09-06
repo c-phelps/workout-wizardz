357c9d06a651a6797cd86064d754a4a74d24e1a3
Halcyon <hashem_elmaghrabi@yahoo.com>
2024-09-05 20:56:32 -0400

    Made changes to exerciseRoutes.js content

--- controllers/api/exerciseRoutes.js
+++ controllers/api/exerciseRoutes.js
@@ -2,38 +2,11 @@ const router = require('express').Router();
 const { Exercise } = require('../../models');
 const withAuth = require('../utils/auth');
 
-router.post('/', async (req, res) => {
-    try{
-        const exerciseData = await Exercise.findAll({
-            include: [
-                {
-                    model: Exercise,
-                    attributes: ['name'],
-                },
-            ],
-        });
-        const exercises = exerciseData.map((exercise) => exercise.get({plain: true}));
-        res.render('homepage', {
-            exercises,
-            logged_in: req.session.logged_in
-        });
-    } catch (err) {
-        res.status(500).json(err);
-    }
-});
-
-router.get('/exercise/:id', async (req, res) => {
+router.get('/exercise/:musclegroup', async (req, res) => {
     try {
-      const exerciseData = await Exercise.findByPk(req.params.id, {
-        include: [
-          {
-            model: Exercise,
-            attributes: ['name'],
-          },
-        ],
-      });
+      const exerciseData = await Exercise.findAll({group: 'musclegroup'});
   
-      const exercise = exerciseData.get({ plain: true });
+      const exercise = exerciseData.map((exe) => exe.get({ plain: true }));
   
       res.render('exercise', {
         ...exercise,
@@ -44,15 +17,4 @@ router.get('/exercise/:id', async (req, res) => {
     }
   });
   
-  router.get('/login', (req, res) => {
-    // If the user is already logged in, redirect the request to another route
-    if (req.session.logged_in) {
-      res.redirect('/profile');
-      return;
-    }
-  
-    res.render('login');
-  });
-  
   module.exports = router;
-  
